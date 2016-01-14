585a3b3b565438e4af14cd85333c5e859b15584f
nagafon <nagafon@mera.ru>
2016-01-14 23:52:36 +0300

    added primitive controls

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
 var renderer = null;
 var scene = null;
 var rect = null;
+var rectVX = 10;
+var rectVY = 10;
+
+var keyboard = {
+    KEYS: {
+        UP: 38,
+        DOWN: 40,
+        LEFT: 37,
+        RIGHT: 39
+    },
+    pressed: []
+};
+
+function bindKeys () {
+    window.addEventListener('keydown', function (e) {
+        keyboard.pressed[e.which] = true;
+    });
+
+    window.addEventListener('keyup', function (e) {
+        keyboard.pressed[e.which] = false;
+    });
+};
+
+function update () {
+    if(keyboard.pressed[keyboard.KEYS.LEFT]) {
+        rect.x -= rectVX;
+    }
+    if(keyboard.pressed[keyboard.KEYS.RIGHT]) {
+        rect.x += rectVX;
+    }
+    if(keyboard.pressed[keyboard.KEYS.UP]) {
+        rect.y -= rectVY;
+    }
+    if(keyboard.pressed[keyboard.KEYS.DOWN]) {
+        rect.y += rectVY;
+    }
+};
 
 function mainLoop () {
     requestAnimationFrame(mainLoop);
+    update();
     renderer.render(scene);
 };
 
@@ -19,7 +57,7 @@ function init () {
     rect.x = 170;
     rect.y = 170;
     scene.addChild(rect);
-
+    bindKeys();
     requestAnimationFrame(mainLoop);
 };
 
